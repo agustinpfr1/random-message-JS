50bdad6c78be1f35059e54d5de72a46fdc4b2119
agustinpfr1 <100004503+agustinpfr1@users.noreply.github.com>
2023-04-14 12:15:03 -0300

    changed obj name

--- js/random-message.js
+++ js/random-message.js
@@ -1,15 +1,17 @@
-let subjects = ["Someone you know", "A new person", "Someone close to you", "An ex friend", "A close friend", "A relative", "A stranger"];
-let futures = ["is going to", "might", "could", "will try to"];
-let actions = ["poke you with", "damage you with", "assassinate you with", "tell you about", "make you cry about", "fight with you for"];
-let objects = ["a hamburger", "a cactus", "your doctor", "an elephant", "Dolly Parton", "a chihuahua", "a rabied raccoon", "a senior developer"];
-
-let buildRandomPhrase = (subject, future, action, object) => {
-    let sub = Math.floor(Math.random() * subject.length);
-    let fut = Math.floor(Math.random() * future.length);
-    let act = Math.floor(Math.random() * action.length);
-    let obj = Math.floor(Math.random() * object.length);
-
-    return `${subject[sub]} ${future[fut]} ${action[act]} ${object[obj]}`
+let randomizedMessage = {
+    subjects: ["Someone you know", "A new person", "Someone close to you", "An ex friend", "A close friend", "A relative", "A stranger"],
+    futures: ["is going to", "might", "could", "will try to"],
+    actions: ["poke you with", "damage you with", "assassinate you with", "tell you about", "make you cry about", "fight with you for"],
+    objects: ["a hamburger", "a cactus", "your doctor", "an elephant", "Dolly Parton", "a chihuahua", "a rabied raccoon", "a senior developer"],
+   
+    buildRandomPhrase(subject, future, action, object) {
+        let sub = Math.floor(Math.random() * this.subjects.length);
+        let fut = Math.floor(Math.random() * this.futures.length);
+        let act = Math.floor(Math.random() * this.actions.length);
+        let obj = Math.floor(Math.random() * this.objects.length);
+        return `${this.subjects[sub]} ${this.futures[fut]} ${this.actions[act]} ${this.objects[obj]}`
+    }
 }
 
-console.log(buildRandomPhrase(subjects, futures, actions, objects))
+
+console.log(randomizedMessage.buildRandomPhrase(randomizedMessage.subjects, randomizedMessage.futures, randomizedMessage.actions, randomizedMessage.objects))
